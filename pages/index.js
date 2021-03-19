5b028343d986b3ada985b05abbdb96de3a082247
Chuka <b.chuka@gmail.com>
2021-03-19 18:15:25 +0800

    update hiiv

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,34 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
-import MyNavbar from "components/my-navbar";
-import Intro from "components/intro";
 import ListItem from "components/list-item";
 import GridItem from "components/grid-item";
 
 import { getAllPosts } from "lib/api";
+import Intro from "components/intro";
+import Layout from "components/layout";
 
 export default function Home({ posts }) {
   return (
-    <Container>
-      <MyNavbar />
-      <div className="blog-detail-page">
-        <Row>
-          <Col md="8">
-            <Intro />
-          </Col>
-        </Row>
-        <pre>{JSON.stringify(posts, null, 2)}</pre>
-        <hr />
-        <div className={`page-wrapper`}>
-          <Row className="mb-5">
-            {/* <Col md="10">
+    <Layout>
+      <Row>
+        <Col md="8">
+          <Intro />
+        </Col>
+      </Row>
+
+      <hr />
+      {/* <pre>{JSON.stringify(posts, null, 2)}</pre> */}
+      <Row className="mb-5">
+        {/* <Col md="10">
               <ListItem />
             </Col> */}
-            {posts.map((post) => (
-              <Col md="4">
-                <GridItem post={post} />
-              </Col>
-            ))}
-          </Row>
-        </div>
-      </div>
-      <footer className="page-footer">
-        <div>
-          <a href="#">нүүр</a>
-          {" | "}
-          <a href="#">фэйсбүүк</a>
-          {" | "}
-        </div>
-      </footer>
-    </Container>
+        {posts.map((post) => (
+          <Col md="4">
+            <GridItem post={post} />
+          </Col>
+        ))}
+      </Row>
+    </Layout>
   );
 }
 
